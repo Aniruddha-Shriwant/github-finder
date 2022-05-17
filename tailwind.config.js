aec987201a1c3f134823d4014d93261df8367027
Restyled.io <commits@restyled.io>
2022-05-17 11:10:39 +0000

    Restyled by prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content : [ "./src/**/*.{html,ts}" ],
-  theme : {
-    extend : {},
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
   },
-  plugins : [],
+  plugins: [],
 };
