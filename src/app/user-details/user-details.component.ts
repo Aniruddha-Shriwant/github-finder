8aabc9d7d06ca3717d5b6dd54297241133a33710
Aniruddha Shriwant <aniruddhashriwant@gmail.com>
2022-05-17 19:12:18 +0530

    User Detail Component finished
    Signed-off-by: Aniruddha Shriwant <aniruddhashriwant@gmail.com>

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -2,6 +2,14 @@ import { Component, OnChanges, Input } from '@angular/core';
 
 import { GithubService } from '../service/github.service';
 
+import {
+  faLocationDot,
+  faEnvelope,
+  faLink,
+} from '@fortawesome/free-solid-svg-icons';
+
+import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
@@ -9,6 +17,16 @@ import { GithubService } from '../service/github.service';
 })
 export class UserDetailsComponent implements OnChanges {
   @Input() username!: string;
+
+  locationIcon = faLocationDot;
+  emailIcon = faEnvelope;
+  blogIcon = faLink;
+  twtIcon = faTwitter;
+  ghIcon = faGithub;
+
+  mailUrl = 'mailto:';
+  twtUrl = 'https://twitter.com/';
+  blogUrl = 'https://';
 
   userInfo: any = null;
   fetchingInfo: boolean = true;
